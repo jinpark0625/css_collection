1059e7544c255e2d9d75512b8ee6328b6dd451db
jin park <jinpark0625@gmail.com>
2022-08-05 00:40:41 -0400

    [MODFIY]Simplify code and remove obsolete checks

--- src/three/surfaceSampling.jsx
+++ src/three/surfaceSampling.jsx
@@ -8,63 +8,56 @@ import { OrbitControls } from "@react-three/drei";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { StyledTHREEContainer } from "../styles/styles";
 
-const Sphere = ({ SetSphereRef, meshSampler }) => {
-  const spref = useRef();
-  const count = 300;
-  const tempPosition = new THREE.Vector3();
-  const tempObject = new THREE.Object3D();
-
-  useEffect(() => {
-    SetSphereRef(spref);
-  }, []);
-
-  useLayoutEffect(() => {
-    // Set positions
-    if (meshSampler && spref) {
-      for (let i = 0; i < 300; i++) {
-        // sample a random point on the surface of the cube & 상장를 표본으로 만들기, 랜덤한 위치의 표본들이 생성
-        meshSampler.sample(tempPosition);
-        // store that point coordinates in the dummy object & 다양한 점들의 좌표들을 저장, 샘플러들로부터 받은 위치를 선정
-        tempObject.position.set(tempPosition.x, tempPosition.y, tempPosition.z);
-        // define a random scale, 구의 크기를 랜덤으로 생성 0 ~ 1 까지의 수를 생성
-        tempObject.scale.setScalar(Math.random() * 0.5 + 0.5);
-        //update the matrix of the object
-        tempObject.updateMatrix();
-        //insert the object updated matrix into our instancedMesh matrix
-        spref.current.setMatrixAt(i, tempObject.matrix);
-      }
-      // Update the instance
-      spref.current.instanceMatrix.needsUpdate = true;
-    }
-  }, [meshSampler]);
-
-  return (
-    <instancedMesh ref={spref} args={[null, null, count]}>
-      <sphereGeometry args={[0.05, 6, 6]} />
-      <meshBasicMaterial color={0xffa0e6} />
-    </instancedMesh>
-  );
-};
-
 const Sampler = ({ SetSamplerRef, setMeshSampler }) => {
   useFrame(({ clock }) => {
-    cubeRef.current.rotation.y = clock.getElapsedTime() * 0.1;
+    meshToSampleRef.current.rotation.y = clock.getElapsedTime() * 0.1;
   });
 
-  const cubeRef = useRef();
+  const meshToSampleRef = useRef();
 
-  useLayoutEffect(() => {
-    SetSamplerRef(cubeRef);
-    setMeshSampler(new MeshSurfaceSampler(cubeRef.current).build());
+  const instancedRef = useRef();
+
+  const count = 300;
+
+  useEffect(() => {
+    if (meshToSampleRef.current === "undefined") return;
+    if (instancedRef.current === "undefined") return;
+
+    const samplerKnot = new MeshSurfaceSampler(meshToSampleRef.current);
+    samplerKnot.build();
+
+    const tempPosition = new Vector3();
+    const tempObject = new Object3D();
+
+    // updates the global transform of the object and its descendants
+    meshToSampleRef.current.updateMatrixWorld(true);
+
+    for (let i = 0; i < count; i++) {
+      // sample a random point on the surface of the cube & 상장를 표본으로 만들기, 랜덤한 위치의 표본들이 생성
+      samplerKnot.sample(tempPosition);
+      // store that point coordinates in the dummy object & 다양한 점들의 좌표들을 저장, 샘플러들로부터 받은 위치를 선정
+      tempObject.position.set(tempPosition.x, tempPosition.y, tempPosition.z);
+      // define a random scale, 구의 크기를 랜덤으로 생성 0 ~ 1 까지의 수를 생성
+      tempObject.scale.setScalar(Math.random() * 0.5 + 0.5);
+      //update the matrix of the object
+      tempObject.updateMatrix();
+      //insert the object updated matrix into our instancedMesh matrix
+      instancedRef.current.setMatrixAt(i, tempObject.matrix);
+    }
+    // Update the instance
+    instancedRef.current.instanceMatrix.needsUpdate = true;
   }, []);
 
   return (
-    <>
-      <mesh ref={cubeRef}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial color={0x66ccff} wireframe={true} />
+    <group>
+      <mesh ref={meshToSampleRef}>
+        <torusKnotGeometry args={[4, 1.3, 100, 16]} />
       </mesh>
-    </>
+      <instancedMesh ref={instancedRef} args={[null, null, count]}>
+        <sphereGeometry args={[0.05, 6, 6]} />
+        <meshBasicMaterial color={0xffa0e6} />
+      </instancedMesh>
+    </group>
   );
 };
 
@@ -79,12 +72,6 @@ const SurfaceSampling = () => {
       height: window.innerHeight,
     });
   }, []);
-
-  const [meshSampler, setMeshSampler] = useState();
-  const [samplerRef, SetSamplerRef] = useState();
-
-  const [sphereRef, SetSphereRef] = useState();
-
   /**
    * playing with particles
    */
@@ -112,13 +99,8 @@ const SurfaceSampling = () => {
         gl={{ antialias: false }}
       >
         <OrbitControls />
-        <group>
-          <Sampler
-            SetSamplerRef={SetSamplerRef}
-            setMeshSampler={setMeshSampler}
-          />
-          <Sphere SetSphereRef={SetSphereRef} meshSampler={meshSampler} />
-        </group>
+
+        <Sampler />
       </Canvas>
     </StyledTHREEContainer>
   );
