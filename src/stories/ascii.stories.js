e43ce2c89314f21d835a110de66b8eaa0ce55125
jin park <jinpark0625@gmail.com>
2022-08-12 17:42:37 -0400

    [ADD]Add asciiEffect in storybook

---
+++ src/stories/ascii.stories.js
@@ -0,0 +1,11 @@
+import { Ascii } from "../three";
+
+export default {
+  title: "three/AsciiEffect",
+  component: Ascii,
+};
+
+const Template = (args) => <Ascii {...args} />;
+
+export const AsciiEffect = Template.bind({});
+AsciiEffect.args = {};
